302b382559a12ebed2e37b9a7e648fd7732df71e
Vegim Hasani <vegimhasani@gmail.com>
2022-11-20 14:15:51 +0100

    Unused import

--- app/src/main/java/com/vegimhasani/rabobank/main/MainFragment.kt
+++ app/src/main/java/com/vegimhasani/rabobank/main/MainFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
