2de225e0b583f63ce29c2152c895e0edb39b5595
Vegim Hasani <vegimhasani@gmail.com>
2023-03-17 10:56:41 +0100

    Renaming

--- app/src/androidTest/java/com/vegimhasani/rabobank/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vegimhasani/rabobank/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.vegimhasani.dott", appContext.packageName)
+        assertEquals("com.vegimhasani.rabobank", appContext.packageName)
     }
 }
